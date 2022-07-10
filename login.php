b10e5b1c185cfe10ebac3bc4a6db66d374d61539
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-07-10 14:30:44 +0300

    added other placeholders, and and some PHP code for the user input validation

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+
+     $username = $_POST['username'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+
+//    test reasons
+echo  $username;
+
+}
+
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,21 +31,21 @@
     <h1 class="text-center">SubmitIT!, your own ticketing portal, to submit requests or incidents, for your technical needs!</h1>
     <h2 class="text-center p-3">Please login to the page to create the tickets</h2>
     <div class="container-sm p-2">
-        <form action="index.php" method="POST">
+        <form action="login.php" method="POST">
                         <div class="mb-3">
                     <label for="Username" class="form-label">Username</label>
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            <input type="text" class="form-control" placeholder="your Username" aria-label="Username" aria-describedby="basic-addon1" name="username">
             </div>
                 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='example: sara@gmail.com' name="email">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary" name='submit'>Submit</button>
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
