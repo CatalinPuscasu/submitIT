61ee9bf7ed9542934809ce76bac05364b5ea63a7
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-07-11 22:50:56 +0300

    added the modal for the registration of users

--- login.php
+++ login.php
@@ -31,22 +31,67 @@ echo  $username;
     <h1 class="text-center">SubmitIT!, your own ticketing portal, to submit requests or incidents, for your technical needs!</h1>
     <h2 class="text-center p-3">Please login to the page to create the tickets</h2>
     <div class="container-sm p-2">
+        <!-- -------------------------------FORM----------------------- -->
         <form action="login.php" method="POST">
                         <div class="mb-3">
                     <label for="Username" class="form-label">Username</label>
             <input type="text" class="form-control" placeholder="your Username" aria-label="Username" aria-describedby="basic-addon1" name="username">
             </div>
+            <!-- ---------------------------------------------------------- -->
                 <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='example: sara@gmail.com' name="email">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
+  <!------------------------------------------------------------------------- -->
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1" name="password">
   </div>
-  <button type="submit" class="btn btn-primary" name='submit'>Submit</button>
+  <!-- ------------------------------------------------------------------------------------ -->
+  <button type="submit" class="btn btn-primary mb-5" name='submit'>Submit</button>
         </form>
+
+        <!-- modal button---------------------------------------------------- -->
+
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Don't have an account yet? Register one!
+</button>
+<!-- ---------------------FORM END---------------------------------------------------- -->
+
+        <!-- MODAL------------------------------- -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Registration</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+          <div class="mb-3">
+                    <label for="Username" class="form-label">Username</label>
+            <input type="text" class="form-control" placeholder="your Username" aria-label="Username" aria-describedby="basic-addon1" name="username">
+            </div>
+            <!-- ---------------------------------------------------------- -->
+                <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='example: sara@gmail.com' name="email">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <!------------------------------------------------------------------------- -->
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+  </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Create an account!</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- --------------------------------------------------------------------------- -->
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
