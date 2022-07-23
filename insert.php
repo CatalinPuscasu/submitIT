552e18e22fb02c277284c4e1c5a27b24a2716eb3
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-07-23 17:04:47 +0300

    added the insert page, for the toastr function to work

---
+++ insert.php
@@ -0,0 +1,39 @@
+<?php 
+
+function registerUser () {
+
+  global $dbConnection;
+
+
+  if (isset($_POST['register'])) {
+
+     $username = $_POST['username'];
+     $email = $_POST['email'];
+     $password = $_POST['password'];
+
+    
+     $query = "INSERT INTO users(username, user_password, user_email, creation_date) VALUES('{$username}', '{$password}', '{$email}', now() ) ";
+
+     $create_users_query = mysqli_query($dbConnection, $query);
+
+     if(!$create_users_query) {
+
+        die('N-a mers!' . mysqli_error($dbConnection));
+     }  
+
+      else {
+        echo "<script type='text/javascript'>toastr.success('Your account has been successfully created!')</script>";
+      }
+     
+     
+      
+   
+
+     }
+
+}
+
+registerUser();
+
+
+?>
