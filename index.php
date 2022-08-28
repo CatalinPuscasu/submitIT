2a4dc2d9bea7ad8232647e7206c7369db357a9d5
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-08-28 19:02:47 +0300

    added the session also on the main page, and displayed the username

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SubmitIT!</title>
+    <h1>Welcome <?php echo $_SESSION['username']; ?></h1>
 </head>
 <body>
     
