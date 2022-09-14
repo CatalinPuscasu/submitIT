d4092361ecbb97d795df5eac3e951af5629f5b78
CatalinPuscasu <puscasucatalin437@gmail.com>
2022-09-15 00:17:46 +0300

    added the incident form page

---
+++ incidentForm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>SubmitIT! Main page</title>
+</head>
+<body>
+    <h1 class="text-center">Use this form to submit your incident!</h1>
+        
+    <form action="">
+        <div class="form-floating">
+  <input type="text" class="form-control" id="Username" placeholder="Username">
+  <label for="Username">Username</label>
+</div>
+        <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+
+    </form>
+    
+</body>
+</html>
